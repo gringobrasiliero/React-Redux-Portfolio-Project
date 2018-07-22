4c28571e055a6e99a3c01f16490e34c58412fb4f
Nolan Byrnes <nolanehs@gmail.com>
2018-07-22 10:57:17 -0400

    Changed some CSS, and began working on NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+export class Header extends Component {
+  render() {
+    return(
+      <div>
+      <h1 className="App-Title">Neddit</h1>
+
+      <div id="nav-bar">
+      <ul>
+      <li>All</li>
+      <li>Dogs</li>
+      <li>Cats</li>
+      </ul>
+    </div>
+      </div>
+    )
+  }
+
+}
+
+
+
+
+
+
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+
+
+            <Header />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
