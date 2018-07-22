dd59266a39f5755644678f5ef040d18a9c9911cf
Nolan Byrnes <nolanehs@gmail.com>
2018-07-22 13:33:21 -0400

    Writing tests for models

--- db/migrate/20180722152134_create_categories.rb
+++ db/migrate/20180722152134_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
-
+      t.string :category
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180722151108) do
+ActiveRecord::Schema.define(version: 20180722152134) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
