7f28ebc7c9dd0d1c139e6317115289164fe8b2e7
Nolan Byrnes <nolanehs@gmail.com>
2018-07-23 07:43:18 -0400

    added routes for users and categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+resources :users
+
+resources :posts do
+  resources :comments
+end
+
   resources :categories do
     resources :posts
   end
