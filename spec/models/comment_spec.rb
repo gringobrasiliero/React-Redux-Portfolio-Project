b2eabfef46c30c312ba582c0b831c154beed3137
Nolan Byrnes <nolanehs@gmail.com>
2018-07-23 05:04:39 -0400

    Validation Testing for User and Comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,4 +7,6 @@ it { should belong_to(:user).dependent(:destroy) }
 it { should belong_to(:post).dependent(:destroy) }
 
 # Validation Testing
+  it { should validate_presence_of(:comment) }
+    it { should validate_presence_of(:created_at) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # Model Testing
 it { should have_many(:posts).dependent(:destroy) }
 it { should have_many(:comments).dependent(:destroy) }
+
+# Validation testing
+  it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:username) }
 end
