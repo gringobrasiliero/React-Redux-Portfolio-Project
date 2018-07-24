781f31b4cb3355dcc3b8dcf644aeb64e9b2b65ae
Nolan Byrnes <nolanehs@gmail.com>
2018-07-23 21:26:45 -0400

    Votes Models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   belongs_to :category, dependent: :destroy
   belongs_to :user
   has_many :comments
+  has_many :votes
   validates_presence_of :post, :created_at
 end
