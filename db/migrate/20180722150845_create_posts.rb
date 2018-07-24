9db347266740cde65fb090fa2f8bb7f65da5b655
Nolan Byrnes <nolanehs@gmail.com>
2018-07-24 09:07:59 -0400

    Update Associations

--- db/migrate/20180722150845_create_posts.rb
+++ db/migrate/20180722150845_create_posts.rb
@@ -2,8 +2,14 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :post
+      t.string :title
+      t.string :description
+      t.string :url
+      t.string :urlToImage
+      t.string :publishedAt
+
       t.references :category, foreign_key: true
-      t.references :user, foreign_key: true
+    
       t.timestamps
     end
   end
