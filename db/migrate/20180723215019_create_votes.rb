4033427c3a9da1f45b99f4a4c5c674d164fbd977
Nolan Byrnes <nolanehs@gmail.com>
2018-07-24 04:19:31 -0400

    references for votes

--- db/migrate/20180723215019_create_votes.rb
+++ db/migrate/20180723215019_create_votes.rb
@@ -4,6 +4,8 @@ class CreateVotes < ActiveRecord::Migration[5.1]
       t.integer :upvote
       t.integer :downvote
       t.boolean :likes
+      t.references :post, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
