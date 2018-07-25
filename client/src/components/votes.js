79e87e93083a53b38038f20157bc4f351713a3cc
Nolan Byrnes <nolanehs@gmail.com>
2018-07-25 04:39:19 -0400

    Started Votes component

--- client/src/components/votes.js
+++ client/src/components/votes.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react';
-import voteReducer from '../reducers/index'
-import { store } from '../createStore'
 
 export default class Vote extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
-      upvote: 0,
-      downvote: 0,
-      score: 0
+
     };
   }
-
-
-
-handleUpvote = () => {
-store.dispatch({type: 'UP_VOTE'})
-}
-
-handleDownvote = () => {
-  store.dispatch({type: 'DOWN_VOTE'})
-
-}
-
 
   componentDidMount() {
     this.fetchVotes()
@@ -31,14 +15,14 @@ handleDownvote = () => {
   fetchVotes = () => {
     fetch('/votes')
       .then(response => response.json())
-      .then(votes => this.setState({votes}))
+      .then(votes => console.log(votes));
   }
 
 render() {
   return(
 <div id="votes">
 
-<Vote  votes={this.props.votes} />
+
 </div>
   );
 }
