2f9bb5c4831a95260c3857c23ffcea87a69e6d26
Nolan Byrnes <nolanehs@gmail.com>
2018-07-25 04:38:58 -0400

    Posts Component.
    collects all posts. kinda.

---
+++ client/src/components/posts.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+export default class Posts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchPosts()
+  }
+  fetchPosts = () => {
+    fetch('posts')
+      .then(response => response.json())
+      .then(posts => this.setState({posts}));
+  }
+
+  renderPosts = () => {
+    return this.state.posts.map(postss => {
+      return(
+  <div>
+        <li key={postss.id}> {postss.post}</li>
+      </div>
+      )
+    })
+  }
+
+
+
+  render() {
+    return(
+  <div id="posts">
+  <ul>
+  {this.renderPosts()}
+  </ul>
+  </div>
+    )
+  }
+
+
+  }
