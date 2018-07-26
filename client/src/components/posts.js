93d5d6f33d3b665350b3829b947937ea314e3a08
Nolan Byrnes <nolanehs@gmail.com>
2018-07-26 05:38:06 -0400

    Seperated concerns in Posts.js

--- client/src/components/posts.js
+++ client/src/components/posts.js
@@ -1,50 +1,50 @@
 import React, { Component } from 'react';
+import Vote from './votes';
+
+const Post = ({ id, title, description, url, publishedAt, urlToImage}) => (
+  <div className="post" key={id} id="posts-list">
+<section>
+
+          <h2> {id}</h2>
+        <h2> {title}</h2>
+        <h2> {description}</h2>
+        <h2> {url}</h2>
+                <h2> {publishedAt}</h2>
+<Vote />
+        <img src="{postss.urlToImage}" />
+</section>
+      </div>
+)
+
+const PostList = ({ posts }) => (
+  <div className="post-list">
+{ posts.map(post => <Post id={post.id} title={post.title} description={post.description} url={post.url} publishedAt={post.publishedAt} urlToImage={post.urlToImage} />) }
+  </div>
+)
+
+
 
 export default class Posts extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
       posts: []
     };
   }
 
-  componentDidMount() {
-    this.fetchPosts()
-  }
   fetchPosts = () => {
     fetch('/posts')
       .then(response => response.json())
       .then(posts => this.setState({posts}));
   }
 
-  renderPosts = () => {
-    return this.state.posts.map(postss => {
-      return(
-  <div key={postss.id} id="posts-list">
-        <h2> {postss.title}</h2>
-        <h2> {postss.description}</h2>
-        <h2> {postss.url}</h2>
-                <h2> {postss.publishedAt}</h2>
-
-        {postss.urlToImage}
-
-      </div>
-      )
-    })
-  }
-
-
+    componentDidMount() {
+      this.fetchPosts()
+    }
 
   render() {
-    return(
-  <div id="posts">
-  <ul>
-  {this.renderPosts()}
-  </ul>
-  </div>
-    )
+    return <PostList posts={this.state.posts} />
   }
-
 
   }
