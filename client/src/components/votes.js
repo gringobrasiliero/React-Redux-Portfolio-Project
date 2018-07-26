cb058152f9cd638be5042192a8501b8fe076f7a2
Nolan Byrnes <nolanehs@gmail.com>
2018-07-26 04:50:13 -0400

    Upvote, Downvote, and Score initial state set plus buttons change state

--- client/src/components/votes.js
+++ client/src/components/votes.js
@@ -5,9 +5,26 @@ export default class Vote extends Component {
     super();
 
     this.state = {
-
+      upvotes: 0,
+      downvotes: 0,
+      score: 0
     };
   }
+
+handleUpvote = () => {
+  this.setState({
+    upvotes: this.state.upvotes += 1,
+    score: this.state.score +=1
+  }, () => this.state.upvotes)
+}
+
+handleDownvote = () => {
+  this.setState({
+    upvotes: this.state.upvotes -= 1,
+    score: this.state.score -=1
+  }, () => this.state.downVotes)
+}
+
 
   componentDidMount() {
     this.fetchVotes()
@@ -21,6 +38,17 @@ export default class Vote extends Component {
 render() {
   return(
 <div id="votes">
+
+<button onClick={this.handleUpvote} bsstyle="primary" bssize="large">
+     Up Vote {this.state.upvotes}
+   </button>
+   <button  bsstyle="primary" bssize="large">
+        {this.state.score}
+      </button>
+
+   <button onClick={this.handleDownvote} bsstyle="primary" bssize="large">
+     Down Vote {this.state.downvotes}
+   </button>
 
 
 </div>
