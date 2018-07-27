ac074cec6f4251ed2deb820d76a3a4cb2d71d3a3
Nolan Byrnes <nolanehs@gmail.com>
2018-07-27 05:19:51 -0400

    Importing Components. requiring dotenv

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Categories from './components/categories';
 import Header from './components/header';
 import Posts from './components/posts';
+import Vote from './components/votes';
+
+import SearchablePostsContainer from './components/searchablePostsContainer';
+require('dotenv').config();
 class App extends Component {
 
 
 
   render() {
     return (
+        <body>
       <div className="App">
         <header className="App-header">
-
-
-
             <Header />
-<Categories />
-<Posts />
+
+<SearchablePostsContainer />
         </header>
+
+
+        <Posts />
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+        </body>
     );
   }
 }
