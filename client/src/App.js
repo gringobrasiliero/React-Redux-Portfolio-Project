775a5156e20a8c90e9e8ea4a6ca2eb39779e04ba
Nolan Byrnes <nolanehs@gmail.com>
2018-07-27 05:19:51 -0400

    Importing Components. requiring dotenv

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import './App.css';
 import Categories from './components/categories';
 import Header from './components/header';
 import Posts from './components/posts';
+import Vote from './components/votes';
+
+import SearchablePostsContainer from './components/searchablePostsContainer';
+require('dotenv').config();
 class App extends Component {
 
 
@@ -20,16 +24,17 @@ class App extends Component {
          <Route exact path="/categories/:id/posts" component={Category} />
       <Route exact path="/login" component={LoginForm} />
 
-
-
-            <Header />
-<Categories />
-<Posts />
+<SearchablePostsContainer />
         </header>
+
+
+        <Posts />
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+        </body>
     );
   }
 }
