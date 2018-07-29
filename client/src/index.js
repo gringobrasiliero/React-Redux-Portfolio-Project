42b9dd95bd9eff21bca0ad7486a92bd72189022e
Nolan Byrnes <nolanehs@gmail.com>
2018-07-29 07:48:55 -0400

    Created Home Page

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import './index.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header';
 import Login from './login';
+import Categories from './components/categories';
 import Home from './Home';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import {applyMiddleware, compose, combineReducers, createStore} from 'redux'
-import thunkMiddleware from 'redux-thunk';
-import logger from 'redux-logger';
 
-import rootReducer from './reducers';
+const Index = () => {
+  return (
+    <div>
+
+
+    </div>
+  );
+};
+
+
+
+
+
+ReactDOM.render(
+<Router>
+<React.Fragment>
+  <Route path="/" component={Header} />
+     <Route exact path="/" component={Home} />
+   <Route exact path="/index" component={Index} />
+<Route exact path="/login" component={Login} />
+<Route exact path="/categories" component={Categories} />
+
+   </React.Fragment>
+  </Router>,
+   document.getElementById('root'));
+registerServiceWorker();
 
 
 const allStoreEnhancers = compose(applyMiddleware(logger, thunkMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
