fe41995570d98596ad1cc56943b7f59997e463d2
Nolan Byrnes <nolanehs@gmail.com>
2018-07-29 07:48:19 -0400

    Rendering articles onto page

--- client/src/components/searchablePostsContainer.js
+++ client/src/components/searchablePostsContainer.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import Posts from './posts';
-import PostLists from './posts';
+// import Posts from './posts';
+// import PostLists from './posts';
+import Vote from './votes';
 
 const NewsAPI = require('newsapi');
 
 const newsapi = new NewsAPI(process.env.REACT_APP_NEWS_SECRET_KEY);
+const apiKey = process.env.REACT_APP_NEWS_SECRET_KEY
 
+const Post = ({ id, title, description, url, publishedAt, urlToImage}) => (
+  <div className="post" key={id} id="posts-list">
+<section>
 
+          <h2> {id}</h2>
+        <h2> {title}</h2>
+        <h2> {description}</h2>
+        <h2> {url}</h2>
+                <h2> {publishedAt}</h2>
+<Vote />
+        <img src={urlToImage} />
+</section>
+      </div>
+)
+
+const PostList = ({ posts }) => (
+  <div className="post-list">
+{ posts.map(post => <Post id={post.id} title={post.title} description={post.description} url={post.url} publishedAt={post.publishedAt} urlToImage={post.urlToImage} />) }
+  </div>
+)
 
 export default class SearchablePostsContainer extends React.Component {
 constructor(props) {
@@ -14,7 +35,8 @@ constructor(props) {
 
   this.state = {
     posts: [],
-    searchCat: ''
+    searchCat: 'politics',
+    articles: []
   }
   this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
@@ -40,29 +62,20 @@ handleChange =(e) => {
   })
 }
 handleSubmit(event) {
-    alert(this.state.searchCat);
-    newsapi.v2.topHeadlines({
-    category: `${this.state.searchCat}`,
-    language: 'en',
-    country: 'us'
-  }).then(response => {
-    console.log(response);
-
-
-  });
     event.preventDefault();
-  }
-componentDidMount = () => {
 
-  newsapi.v2.topHeadlines({
-  category: 'politics',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-
-});
+fetch("https://newsapi.org/v2/everything?q=" + `${this.state.searchCat}` + "&apiKey=" + apiKey)
+.then(response => response.json())
+  .then(posts => this.setState({posts: posts.articles})).then(console.log(this.state.posts));
 }
+
+
+componentDidMount() {
+fetch("https://newsapi.org/v2/everything?q=" + `${this.state.searchCat}` + "&apiKey=" + apiKey)
+.then(response => response.json())
+.then(posts => this.setState({posts: posts.articles}));
+}
+
 
 render() {
   return(
@@ -72,7 +85,7 @@ render() {
         <input type='text' value={this.state.searchCat} onChange={this.handleChange} />
         <input type='submit' value="Submit" />
       </form>
-
+ <PostList posts={this.state.posts} />
       </div>
   )
 }
