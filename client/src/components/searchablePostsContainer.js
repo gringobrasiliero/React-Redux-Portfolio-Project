ed3a743a608d8868a53e3bbaddb1407e9c8b35aa
Nolan Byrnes <nolanehs@gmail.com>
2018-07-29 15:47:47 -0400

    Took out useless concatenation

--- client/src/components/searchablePostsContainer.js
+++ client/src/components/searchablePostsContainer.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-// import Posts from './posts';
-// import PostLists from './posts';
+import React from 'react';
+
+// import Articles from './articles';
+// import ArticleLists from './articles';
 import Vote from './votes';
 
 const NewsAPI = require('newsapi');
@@ -8,42 +9,43 @@ const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(process.env.REACT_APP_NEWS_SECRET_KEY);
 const apiKey = process.env.REACT_APP_NEWS_SECRET_KEY
 
-const Post = ({ id, title, description, url, publishedAt, urlToImage}) => (
-  <div className="post" key={id} id="posts-list">
+const Article = ({ id, title, description, url, publishedAt, urlToImage}) => (
+  <div className="article" key={url}>
 <section>
+<img src={urlToImage} alt="" />
+<div className="content">
+          <h2> <a href={url}>{title}</a></h2>
+        <p> {description}</p>
 
-          <h2> {id}</h2>
-        <h2> {title}</h2>
-        <h2> {description}</h2>
-        <h2> {url}</h2>
-                <h2> {publishedAt}</h2>
+              </div>
 <Vote />
-        <img src={urlToImage} />
+
 </section>
+<br />
       </div>
 )
 
-const PostList = ({ posts }) => (
-  <div className="post-list">
-{ posts.map(post => <Post id={post.id} title={post.title} description={post.description} url={post.url} publishedAt={post.publishedAt} urlToImage={post.urlToImage} />) }
+const ArticleList = ({ articles }) => (
+  <div className="article-list">
+{ articles.map(article => <Article id={article.id} title={article.title} description={article.description} url={article.url} publishedAt={article.publishedAt} urlToImage={article.urlToImage} />) }
   </div>
 )
 
-export default class SearchablePostsContainer extends React.Component {
+export default class SearchableArticlesContainer extends React.Component {
 constructor(props) {
   super(props)
 
   this.state = {
-    posts: [],
+    articles: [],
     searchCat: 'politics',
-    articles: []
+
   }
   this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
 
 }
 
-searchPosts = (e) => {
+searchArticles = (e) => {
   e.preventDefault();
 
   newsapi.v2.topHeadlines({
@@ -64,28 +66,28 @@ handleChange =(e) => {
 handleSubmit(event) {
     event.preventDefault();
 
-fetch("https://newsapi.org/v2/everything?q=" + `${this.state.searchCat}` + "&apiKey=" + apiKey)
+fetch(`https://newsapi.org/v2/everything?q=${this.state.searchCat}&apiKey=${apiKey}`)
 .then(response => response.json())
-  .then(posts => this.setState({posts: posts.articles})).then(console.log(this.state.posts));
+  .then(articles => this.setState({articles: articles.articles})).then(console.log(this.state.articles));
 }
 
 
 componentDidMount() {
-fetch("https://newsapi.org/v2/everything?q=" + `${this.state.searchCat}` + "&apiKey=" + apiKey)
+fetch(`https://newsapi.org/v2/everything?q=${this.state.searchCat}&apiKey=${apiKey}`)
 .then(response => response.json())
-.then(posts => this.setState({posts: posts.articles}));
+.then(articles => this.setState({articles: articles.articles}));
 }
 
 
 render() {
   return(
-    <div className='searchable-posts'>
+    <div className='searchable-articles'>
 
      <form onSubmit={this.handleSubmit}>
         <input type='text' value={this.state.searchCat} onChange={this.handleChange} />
         <input type='submit' value="Submit" />
       </form>
- <PostList posts={this.state.posts} />
+ <ArticleList articles={this.state.articles} />
       </div>
   )
 }
