7b54a084a06d6a7c270730f99db99b0a341c29e9
Nolan Byrnes <nolanehs@gmail.com>
2018-07-29 07:48:55 -0400

    Created Home Page

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/header';
+import Login from './login';
+import Categories from './components/categories';
+import Home from './Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index = () => {
+  return (
+    <div>
+
+
+    </div>
+  );
+};
+
+
+
+
+
+ReactDOM.render(
+<Router>
+<React.Fragment>
+  <Route path="/" component={Header} />
+     <Route exact path="/" component={Home} />
+   <Route exact path="/index" component={Index} />
+<Route exact path="/login" component={Login} />
+<Route exact path="/categories" component={Categories} />
+
+   </React.Fragment>
+  </Router>,
+   document.getElementById('root'));
 registerServiceWorker();
