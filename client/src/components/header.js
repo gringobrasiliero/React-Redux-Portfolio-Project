c7900bf0340542b164842f51009ed9616084049c
Nolan Byrnes <nolanehs@gmail.com>
2018-07-29 14:41:46 -0400

    Link to home in header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import Categories from './categories';
 import SearchablePostsContainer from './searchablePostsContainer';
+import { Link } from 'react-router-dom';
+
+
 export default class Header extends Component {
   render() {
     return(
       <div className="App">
           <header className="App-header">
+
+
             <h1 className="App-Title">Neddit</h1>
+            <ul>
+              <li><Link to={'/'}>Home</Link></li>
+            </ul>
+            
             <Categories />
-            <SearchablePostsContainer />
           </header>
       </div>
     )
