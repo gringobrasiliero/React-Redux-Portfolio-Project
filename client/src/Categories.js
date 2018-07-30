6d639077f5140ca094d16c8eb7e9033f7468cd36
Nolan Byrnes <nolanehs@gmail.com>
2018-07-30 03:06:46 -0400

    Made Categories Page

--- client/src/Categories.js
+++ client/src/Categories.js
@@ -1,19 +1,18 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import CategoriesContainer from './components/categories'
+
 import SearchablePostsContainer from './components/searchablePostsContainer';
-import PostsContainer from './containers/posts'
 require('dotenv').config();
 
 
 
 const Categories = () => (
       <div className="App">
-
 <h1>This is my Categories page </h1>
 
-<PostsContainer />
+
+
 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
