e8ef553e73acbcb2bcf99c03d44081ac3a756122
Nolan Byrnes <nolanehs@gmail.com>
2018-07-31 04:04:04 -0400

    Separated post components

---
+++ client/src/components/postComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Vote from './votes';
+
+const Post = ({ id, title, description, url, publishedAt, urlToImage, category_id}) => (
+  <div className="post" key={id}>
+<img src={urlToImage} alt="" />
+  <section>
+
+  <div className="content">
+            <h2> <a href={url}>{title}</a></h2>
+          <p> {description}</p>
+<h1>{category_id}</h1>
+                </div>
+
+
+  </section>
+    <Vote />
+  <br />
+    <br />
+      </div>
+)
+
+export default Post
