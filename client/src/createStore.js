aadb4b73df6691b7c9f020f70092c56d22615de6
Nolan Byrnes <nolanehs@gmail.com>
2018-08-01 07:54:33 -0400

    Creating a Store

---
+++ client/src/createStore.js
@@ -0,0 +1,25 @@
+
+
+export default function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action)
+
+    render();
+
+  };
+
+  function getState() {
+    return state;
+  };
+
+  return{
+    dispatch,
+    getState
+  };
+}
+
+function render() {
+  const container = document.getElementById('container');
+}
