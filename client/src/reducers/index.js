cf800f19a664dd33710fc0ea36438da98fd35abc
Nolan Byrnes <nolanehs@gmail.com>
2018-08-02 06:33:11 -0400

    Added Post Reducer skeleton with default

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import voteReducer from './voteReducer';
 import categoriesReducer from './categoriesReducer'
-
+import postsReducer from './postsReducer'
 export default combineReducers({
   // votes: votesReducer
-categories: categoriesReducer
+categories: categoriesReducer,
+posts: postsReducer
 });
