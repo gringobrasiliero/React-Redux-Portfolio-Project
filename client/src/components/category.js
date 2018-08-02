40af2c22645ecf06e4c61ce26cf65a6e90ebc255
Nolan Byrnes <nolanehs@gmail.com>
2018-08-02 06:24:19 -0400

    Added CategoriesReducer frame. Refactored links for categories

--- client/src/components/category.js
+++ client/src/components/category.js
@@ -4,9 +4,17 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PostsContainer from './posts'
 import PostList from './posts'
+import CategoriesList from './categoriesList'
 
-const Category = ({id, category, key}) => (
-  <li id="catPosts"><Link key={key}  id={id} to={`/categories/${id}/posts`}>{category}</Link></li>
-)
+const Category = ({match, categories}) =>
+  <div>
+    <CategoriesList categories={categories} />
+    <Route path={`${match.url}/:id/posts`} component={PostsContainer}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Select a Category to view some of our communities favorites</h3>
+    )}/>
+</div>
+
+
 
 export default Category
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import voteReducer from './voteReducer';
+import categoriesReducer from './categoriesReducer'
 
 export default combineReducers({
   // votes: votesReducer
+categories: categoriesReducer
 });
