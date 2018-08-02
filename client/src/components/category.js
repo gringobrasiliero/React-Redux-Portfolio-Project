4874f38672110b6415a265fe0fe1f404798d9edf
Nolan Byrnes <nolanehs@gmail.com>
2018-08-02 04:36:10 -0400

    Separated Categories components

--- client/src/components/category.js
+++ client/src/components/category.js
@@ -5,28 +5,8 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PostsContainer from './posts'
 import PostList from './posts'
 
-
-const find = id => PostsContainer.find(p => p.category_id == id);
-
-
-
-const Category = ({match}) => (
-  <div>
-  <h3> {match.params.id}  {find}</h3>
-
-  <PostsContainer posts={find} />
-
-  </div>
-);
-
-
-
-
-
-
-
-
-
-
+const Category = ({id, category, key}) => (
+  <li id="catPosts"><Link key={key}  id={id} to={`/categories/${id}/posts`}>{category}</Link></li>
+)
 
 export default Category
