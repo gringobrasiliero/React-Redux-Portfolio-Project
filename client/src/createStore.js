3a3669c582d7fa8a96d9711e9bc8ec634fe8d7ad
Nolan Byrnes <nolanehs@gmail.com>
2018-08-03 09:40:27 -0400

    Don't need this store

--- client/src/createStore.js
+++ client/src/createStore.js
@@ -1,36 +1,9 @@
 
 import { createStore } from 'redux'
 import rootReducer from './reducers/index'
-
+import { render } from './index.js';
 export function configureStore(){
   return createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 }
 
 export const store = configureStore()
-
-
-
-// import { render } from './index.js';
-//
-// export default function createStore(reducer) {
-//   let state;
-//
-//   function dispatch(action) {
-//     state = reducer(state, action)
-//     console.log(`the state is ${state.count}`)
-//        console.log(`the action is ${action.type}`)
-//     render();
-//
-//   };
-//
-//   function getState() {
-//     return state;
-//   };
-//
-//
-//
-//   return{
-//     dispatch,
-//     getState
-//   };
-// }
