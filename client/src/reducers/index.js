2a2db8931a85bfc664ade0bf9547fa661b5d8b36
Nolan Byrnes <nolanehs@gmail.com>
2018-08-03 05:04:43 -0400

    Added Users Reducer and to store

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux';
 import voteReducer from './voteReducer';
 import categoriesReducer from './categoriesReducer'
 import postsReducer from './postsReducer'
+import usersReducer from './usersReducer'
+
+
 export default combineReducers({
   // votes: votesReducer
 categories: categoriesReducer,
-posts: postsReducer
+posts: postsReducer,
+users: usersReducer
 });
---
+++ client/src/reducers/usersReducer.js
@@ -0,0 +1,13 @@
+export default function usersReducer(state = {
+  username: "",
+  password: "",
+  email: "",
+
+}, action) {
+  switch (action.type) {
+
+
+    default:
+      return state;
+    }
+};
