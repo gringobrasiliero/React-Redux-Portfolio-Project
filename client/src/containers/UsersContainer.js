366ee657a5c47e3ae66d0efbc317db1a9c825bf7
Nolan Byrnes <nolanehs@gmail.com>
2018-08-03 03:46:25 -0400

    dded UsersContainer Skelly

---
+++ client/src/containers/UsersContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export default class User extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: ""
+      username: ""
+
+    };
+  }
+
+
+  componentDidMount() {
+  }
+
+render() {
+  return(
+
+
+  );
+}
+
+
+}
