e9df2c964c1680bf5603eb1162f54460e1c53b4a
Nolan Byrnes <nolanehs@gmail.com>
2018-08-05 13:56:19 -0400

    Login form turned to stateless

--- client/src/components/users/LoginForm.js
+++ client/src/components/users/LoginForm.js
@@ -1,45 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      email: '',
-      password: ''
+const LoginForm = (props) => (
+  <form>
+    <h3>Sign in</h3>
+    <input type="text"  placeholder="enter you username" />
+    <input type="password"  placeholder="enter password" />
+    <input type="submit" value="Login" />
+  </form>
+)
 
-    };
-  }
-
-  handleSignIn(e) {
-    e.preventDefault();
-     this.props.store.dispatch({
-       type: 'SIGNIN',
-       user: {
-         username: this.state.username,
-         email: this.state.email,
-         password: this.state.password,
-
-       }
-     })
-
-     this.setState({
-           username: '',
-           email: '',
-           password: '',
-           })
-       }
-
-
-  render() {
-    return (
-      <form onSubmit={this.handleSignIn.bind(this)}>
-        <h3>Sign in</h3>
-        <input type="text" ref="username" placeholder="enter you username" />
-        <input type="password" ref="password" placeholder="enter password" />
-        <input type="submit" value="Login" />
-      </form>
-    )
-  }
-
-}
+export default LoginForm;
