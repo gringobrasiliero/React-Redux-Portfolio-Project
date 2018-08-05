9317534e2a3881163f5662bc88b37fdd7e960647
Nolan Byrnes <nolanehs@gmail.com>
2018-08-05 13:57:21 -0400

    Took out unneeded imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 import Header from './components/header';
 import Login from './login';
-import Categories from './containers/categories';
 import Home from './Home';
-import PostsContainer from './containers/posts'
-import voteReducer from './reducers/voteReducer'
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import Upvote from './components/votes/Upvote'
 import {applyMiddleware, compose, combineReducers, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
