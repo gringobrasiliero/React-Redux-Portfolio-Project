2b8e63fded94ef67c7b951b273ef97e20a8799e2
Nolan Byrnes <nolanehs@gmail.com>
2018-08-05 14:15:10 -0400

    Prevent repeated posts from showing on page

--- client/src/reducers/postsReducer.js
+++ client/src/reducers/postsReducer.js
@@ -6,8 +6,11 @@ export default (state = initialState.posts, action) => {
   switch(action.type) {
 
     case FETCH_POSTS:
-      return [...state, ...action.payload]
+    let index = state.findIndex(el => el.post_id === action.payload.post_id);
 
+    if(index === -1) {
+      return [...state, ...action.payload];
+}
     default:
       return state
   }
