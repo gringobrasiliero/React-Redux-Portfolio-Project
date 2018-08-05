a3a081da5e85efd297af920b97474293e7ec3502
Nolan Byrnes <nolanehs@gmail.com>
2018-08-05 13:57:21 -0400

    Took out unneeded imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 import App from './App';
+<<<<<<< master
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header';
@@ -34,6 +35,18 @@ ReactDOM.render(
   </Router>,
    document.getElementById('root'));
 registerServiceWorker();
+=======
+import Header from './components/header';
+import Login from './login';
+import Home from './Home';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import {applyMiddleware, compose, combineReducers, createStore} from 'redux'
+import thunkMiddleware from 'redux-thunk';
+import logger from 'redux-logger';
+
+import rootReducer from './reducers';
+>>>>>>> Took out unneeded imports
 
 
 const allStoreEnhancers = compose(applyMiddleware(logger, thunkMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
