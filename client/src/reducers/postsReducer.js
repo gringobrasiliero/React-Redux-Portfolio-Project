51e8e24f1806ad53596d84d06251b3c4ce7713a3
Nolan Byrnes <nolanehs@gmail.com>
2018-08-05 09:24:32 -0400

    Categories will no longer duplicate.

---
+++ client/src/reducers/postsReducer.js
@@ -0,0 +1,14 @@
+import { FETCH_POSTS} from '../actions/posts-actions'
+
+let initialState = []
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+
+    case FETCH_POSTS:
+      return [...state, ...action.payload]
+
+    default:
+      return state
+  }
+}
