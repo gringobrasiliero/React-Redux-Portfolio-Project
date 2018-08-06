ddf5aab43492bc22e104ec22bde8f6d6fb1f31f5
Nolan Byrnes <nolanehs@gmail.com>
2018-08-06 17:03:04 -0400

    Made a POST request function. Haven't tested or connected.

--- client/src/actions/categoryActions.js
+++ client/src/actions/categoryActions.js
@@ -33,15 +33,25 @@ export const fetchCatPosts = () => {
 }
 
 
-export const newCategory = () => {
+export const newCategory = (url = "", data = {}) => {
   return (dispatch) => {
-    fetch(`/categories/:id/posts`)
-      .then(res => res.json())
-      .then(category => {
-        dispatch({
-          type: NEW_CATEGORY,
-          payload: category
-        })
+    fetch(`/categories/:id/posts`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          // "Content-Type": "application/x-www-form-urlencoded",
+      },
+      redirect: "follow", // manual, *follow, error
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+  .then(res => res.json())
+  .then(category => {
+    dispatch({
+      type: NEW_CATEGORY,
+      payload: category
+    }
+
+        )
       })
-  }
+}
 }
