b7e9b88332eb4f931631c93b4f3888d490254c10
Nolan Byrnes <nolanehs@gmail.com>
2018-08-08 05:13:38 -0400

    turned fields to text from string to handle longer urls and urlToPic
    also routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :votes
-
+resources :categories
+resources :posts
 resources :users
 
 resources :posts do
--- db/migrate/20180722150845_create_posts.rb
+++ db/migrate/20180722150845_create_posts.rb
@@ -3,13 +3,13 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.string :post
       t.string :title
-      t.string :description
-      t.string :url
-      t.string :urlToImage
+      t.text :description
+      t.text :url
+      t.text :urlToImage
       t.string :publishedAt
 
       t.references :category, foreign_key: true
-    
+
       t.timestamps
     end
   end
