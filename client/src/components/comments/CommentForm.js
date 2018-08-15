1f0ec6a131220187c3fdf73d7ec397a629e3defb
Nolan Byrnes <nolanehs@gmail.com>
2018-08-15 03:53:58 -0400

    Added Button Listener to Comment form.

--- client/src/components/comments/CommentForm.js
+++ client/src/components/comments/CommentForm.js
@@ -2,14 +2,13 @@ import React from 'react'
 // import { Field, reduxForm } from 'redux-form'
 
 
- let CommentForm = ({ id, comment, user_id, handleNewComment, handleChange}) => (
+ let CommentForm = ({ id, comment, post_id, onHandleSubmit, handleChange}) => (
   <div className="comment" key={id}>
 
-      <form onSubmit={handleNewComment}>
+      <form onSubmit={onHandleSubmit}>
       <label>Comment: </label>
         <input type="text" id="comment" name="comment" />
         <input type="text" id="postId" name="postId" defaultValue={"POST ID GOES HERE"} style={{display: 'none'}} />
-        <input type="text" id="userId" name="userId" defaultValue={"USER ID GOES HERE"} style={{display: 'none'}} />
 <input type="submit" value="Post Your Comment!" />
       </form>
 
