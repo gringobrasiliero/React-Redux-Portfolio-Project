952169d3d55372fb95463934d07d6ff5fb12fe83
Nolan Byrnes <nolanehs@gmail.com>
2018-08-15 03:52:01 -0400

    Took out unneeded Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rails.application.routes.draw do
-devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :votes
+
 resources :categories
 resources :posts
-resources :users
 resources :comments
 
 resources :posts do
   resources :comments
-    resources :votes
+
 end
 
 
 
-resources :users do
-  resources :votes
-  resources :comments
-end
 
   resources :categories do
     resources :posts
