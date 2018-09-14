86bc846bcf83b7ab85ff50191ab17af6c120f445
Nolan Byrnes <nolanehs@gmail.com>
2018-09-14 14:19:06 -0400

    Syntax error. needed comma

--- app/serializers/comments_serializer.rb
+++ app/serializers/comments_serializer.rb
@@ -1,4 +1,4 @@
 class CommentsSerializer < ActiveModel::Serializer
   attributes :id, :comment, :post_id, :created_at, :updated_at
-  belongs_to :posts serializer: PostCommentsSerializer
+  belongs_to :posts, serializer: PostCommentsSerializer
 end
