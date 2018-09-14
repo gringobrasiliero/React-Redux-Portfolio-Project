e0f0d31189090174fe915efe3e31746659eaa50e
Nolan Byrnes <nolanehs@gmail.com>
2018-09-14 14:12:06 -0400

    Debugging for heroku

--- app/serializers/category_post_serializer.rb
+++ app/serializers/category_post_serializer.rb
@@ -1,3 +1,3 @@
-class CategoryPostsSerializer < ActiveModel::Serializer
+class CategoryPostSerializer < ActiveModel::Serializer
 attributes :post_id, :category_id, :post, :title, :description, :url, :urlToImage, :publishedAt, :created_at
 end
