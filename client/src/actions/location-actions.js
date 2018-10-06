4bfdfd3610b590e57e3ac54efca9756e2daefb95
Nolan Byrnes <nolanehs@gmail.com>
2018-10-06 04:40:59 -0400

    Request must be over https to prevent mixed content

--- client/src/actions/location-actions.js
+++ client/src/actions/location-actions.js
@@ -6,7 +6,7 @@ const mapsApiKey = process.env.REACT_APP_KEY;
 export const fetchLocation = (latitude, longitude) => {
 return (dispatch) => {
 
-  fetch("http://maps.googleapis.com/maps/api/geocode/json?latlng="+ latitude + "," + longitude +"&sensor=false&key=" +  mapsApiKey)
+  fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng="+ latitude + "," + longitude +"&sensor=false&key=" +  mapsApiKey)
     .then(res => res.json())
     .then(location => {
       dispatch({
