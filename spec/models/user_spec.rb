73fcb99e391c0e79693c77475323e97b080ea04e
Nolan Byrnes <nolanehs@gmail.com>
2018-11-12 07:55:04 -0500

    Tests for Users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # Model Testing
-
-it { should have_many(:comments).dependent(:destroy) }
-
-# Validation testing
-  it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:username) }
+  pending "add some examples to (or delete) #{__FILE__}"
 end
