3bf42eec7c6687b3a7680669978c23c1a333ef3d
Nolan Byrnes <nolanehs@gmail.com>
2018-11-12 06:27:07 -0500

    Set up async function for signup. doesn't work yet.

--- client/src/actions/user-actions.js
+++ client/src/actions/user-actions.js
@@ -1,10 +1,31 @@
 export const GUEST_LOGIN = 'GUEST_LOGIN';
-
+export const SIGN_UP = 'SIGN_UP';
 export const guestLogin = (guestName) => {
   return (dispatch) => {
     dispatch({
       type: GUEST_LOGIN,
       payload: guestName
     })
+  }
+}
+
+
+export const signUp = (url = "/users", data = {}) => {
+  return (dispatch) => {
+    fetch(url, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json; charset=utf-8",
+      },
+      redirect: "follow", // manual, *follow, error
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+    .then(res => res.json())
+      .then(users => {
+        dispatch({
+          type: SIGN_UP,
+          payload: users
+        })
+      })
   }
 }
