f0cc965b886f82df4e9c80d88eae05673d61edfb
Nolan Byrnes <nolanehs@gmail.com>
2018-12-09 07:25:06 -0500

    Signup returning user info through Redux

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    json_response(@user)
+
   end
 
   def create
--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -1,12 +1,16 @@
 import initialState from './initialState';
 import {GUEST_LOGIN} from '../actions/user-actions'
+import {SIGN_UP} from '../actions/user-actions'
 
 
 export default (state = initialState.users, action) => {
   switch(action.type) {
 
     case GUEST_LOGIN:
-      return [ action.payload];
+      return [action.payload];
+
+    case SIGN_UP:
+        return [action.payload];
 
     default:
       return state
